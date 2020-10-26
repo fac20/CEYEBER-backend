27d6a4a290314d6ee164e067d6b143bf619ca35b
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-10-26 14:53:20 +0000

    connection to database Co-authored-by: Rihards <rihards.jukna@gmail.com>

---
+++ src/database/connection.js
@@ -0,0 +1,12 @@
+const pg = require('pg');
+const env = require('dotenv');
+
+env.config();
+
+const options {
+    connectionString: process.env.DATABASE_URL
+}
+
+const database = new pg.Pool(options);
+
+module.exports = database;
