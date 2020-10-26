fd7fc6ff9a0a786ce7343f2965f5b9b6efd83665
Jessica Deacon <j.r.deacon@hotmail.co.uk>
2020-10-26 16:21:50 +0000

    create route in server Co-authored-by: Rihards <rihards.jukna@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const createUser = require('./handlers/users');
+const sendAnswers = require('./handlers/challenges');
+const sendSkills = require('./handlers/skills');
 
 require('dotenv').config();
 
@@ -8,5 +11,9 @@ const PORT = process.env.PORT || 4000;
 app.get('/', function (req, res) {
   res.send('Hello');
 });
+
+app.post('/user', createUser);
+app.post('/skills', sendSkills);
+app.post('/answers', sendAnswers);
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
