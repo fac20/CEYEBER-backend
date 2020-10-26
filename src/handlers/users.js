6e9d136fcaf60c0be13ca2af6c3109b4d476a814
RihardsJ <rihards.jukna@gmail.com>
2020-10-26 17:36:15 +0000

    fixed users handler Co-authored-by: Jennifer B <jennifer.sesaybarnes@gmail.com>

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -1,8 +1,7 @@
 const user = require('../model/userInfo');
 
 const createUser = (req, res, next) => {
-  const user_id = req.user_id;
-  const alais = req.alais;
+  const alias = req.alias;
   const age = req.age;
   const location = req.location;
   const created_at = req.created_at;
