115e9f0609702eec09f30268e1132d73493836d7
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-10-28 15:58:08 +0000

    Adds error handling middleware to all routes

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ const createUser = require('./handlers/users');
 const sendAnswers = require('./handlers/challenges');
 const sendSkills = require('./handlers/skills');
 const cors = require('cors');
-
 require('dotenv').config();
+
+const handleErrors = require('./middleware/errorHandling');
 
 const PORT = process.env.PORT || 4000;
 
@@ -15,6 +16,7 @@ const corsOptions = {
 };
 
 app.use(express.json());
+app.use(handleErrors);
 app.use(cors());
 
 app.get('/', function (req, res) {
